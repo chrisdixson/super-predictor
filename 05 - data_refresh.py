5381a53c41c6e258ae1cac069f9d3c35bfeb44ae
alastair.chin@au.ey.com <alastair.chin@au.ey.com>
2023-09-27 00:46:04 +0000

    change series years to run from to current year

--- 05 - data_refresh.py
+++ 05 - data_refresh.py
@@ -200,8 +200,8 @@ from pyspark.sql.types import StructType, StructField, StringType, DateType
 master_df_path    = 'dbfs:/FileStore/super-cricket/master_df.parquet'
 initial_pull_date = date(2021, 1, 1)
 end_date          = date.today() - timedelta(days=1)
-series_urls       = get_series_urls(2021) #uncomment to run from custom year
-#series_urls       = get_series_urls(datetime.now().year)
+#series_urls       = get_series_urls(2021) #uncomment to run from custom year
+series_urls       = get_series_urls(datetime.now().year)
 
 schema = StructType([StructField('date', DateType(), True),
                      StructField('first_innings_team', StringType(), True),
